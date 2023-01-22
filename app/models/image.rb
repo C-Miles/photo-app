018226c40473f7134e0a3837e55513a9ec2ddf3d
Chris Miles <cmiles@obm.com>
2023-01-22 13:28:18 -0500

    wip

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,13 @@
+# frozen_string_literal: true
+
 class Image < ApplicationRecord
   belongs_to :user
   mount_uploader :picture, PictureUploader
+  validate :pictue_size
+
+  def pictue_size
+    return unless picture.size > 5.megabytes
+
+    errors.add(:picture, 'should be less than 5MB')
+  end
 end
